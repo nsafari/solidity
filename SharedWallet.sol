1f73bb2af6b3b96a3611b7d150c83963676ad254
nSafari <nassersafari@gmail.com>
2021-06-12 17:52:00 +0200

    Using Ownable contract

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,17 +1,7 @@
 pragma solidity ^0.8.1;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract ShareWallet{
-
-    address owner;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You are not allowed");
-        _;
-    }
+contract ShareWallet is Ownable {
 
     function withdrawMoney(address payable _to, uint256 _amount) public onlyOwner{
         _to.transfer(_amount);
