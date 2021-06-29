57905941efaf4a3e46707f1684820c18ebe81d63
nSafari <nassersafari@gmail.com>
2021-06-29 05:18:56 +0200

    Adding events

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.1;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract ShareWallet is Ownable {
+contract Allowance is Ownable {
 
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
 
 
@@ -10,6 +11,25 @@ contract ShareWallet is Ownable {
         require(isOwner() || allowance[msg.sender] >= _amount, 'You are not allowed');
         _;
     }
+
+    function addAllowance(address _who, uint _amount) public onlyOwner{
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] += _amount;
+    }
+
+    function isOwner() internal view returns (bool) {
+        return owner() == msg.sender;
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] -= _amount;
+    }
+
+
+}
+
+contract ShareWallet is Allowance {
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount){
         require(_amount <= address(this).balance, "There is not enough balance");
@@ -20,17 +40,6 @@ contract ShareWallet is Ownable {
         _to.transfer(_amount);
     }
 
-    function addAllowance(address _who, uint _amount) public onlyOwner{
-        allowance[_who] += _amount;
-    }
-
-    function isOwner() private view returns (bool) {
-        return owner() == msg.sender;
-    }
-
-    function reduceAllowance(address _who, uint _amount) private {
-        allowance[_who] -= _amount;
-    }
 
     receive () external payable {
 
