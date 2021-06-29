e751b1354ae1621d938dee61d167d90cfe5086b5
nSafari <nassersafari@gmail.com>
2021-06-29 04:57:50 +0200

    Adding reduce allowance

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -7,16 +7,29 @@ contract ShareWallet is Ownable {
 
 
     modifier ownerOrAllowed(uint _amount){
-        require(owner() == msg.sender || allowance[msg.sender] >= _amount, 'You are not allowed');
+        require(isOwner() || allowance[msg.sender] >= _amount, 'You are not allowed');
         _;
     }
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount){
+        require(_amount <= address(this).balance, "There is not enough balance");
+        if(!isOwner()){
+            reduceAllowance(msg.sender, _amount);
+        }
+
         _to.transfer(_amount);
     }
 
     function addAllowance(address _who, uint _amount) public onlyOwner{
         allowance[_who] += _amount;
+    }
+
+    function isOwner() private view returns (bool) {
+        return owner() == msg.sender;
+    }
+
+    function reduceAllowance(address _who, uint _amount) private {
+        allowance[_who] -= _amount;
     }
 
     receive () external payable {
